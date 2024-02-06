f9e2c479fcdbc71600e35278a1f689f7d35c615f
Awayume <dev@awayume.jp>
2024-02-06 19:36:44 +0900

    add: Add a main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+// SPDX-FileCopyrightText: 2024 Awayume <dev@awayume.jp>
+// SPDX-License-Identifier: AGPL-3.0-only
+
+#[tokio::main]
+async fn main() {
+    println!("AegisDB server (Version {})", env!("CARGO_PKG_VERSION"));
 }
